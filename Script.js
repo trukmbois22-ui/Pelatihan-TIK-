c148e877397afbc2b66455a27a79820f97f3e798
trukmbois22-ui <trukmbois22@gmail.com>
2025-10-23 10:17:47 +0700

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID- nya 
+const tombolsapa=document.getE1ementById('sapabutton');
+// 2. Tambahan 'event Listener' untuk aksi 'click
+Tombolsapa.addEventListener('click', Function() {
+  //3. Jalankan kode ini ketika tombol di-Klik
+       alert(' Jangan lihat lihat INI WEB PRIBADI');
+}); 
